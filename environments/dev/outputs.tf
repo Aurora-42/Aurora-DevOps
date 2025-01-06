51e32c71c9482181b8ac47677c42bf517b089c97
gsistelos <gsistelos@proton.me>
2025-01-06 16:33:56 -0300

    remove outputs

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -3,7 +3,7 @@ output "github_actions_terraform_credentials" {
   sensitive = true
 }
 
-output "github_actions_acr_push_credentials" {
-  value = module.aurora.github_actions_acr_push_credentials
-  sensitive = true
-}
+# output "github_actions_acr_push_credentials" {
+#   value = module.aurora.github_actions_acr_push_credentials
+#   sensitive = true
+# }
--- modules/aurora/outputs.tf
+++ modules/aurora/outputs.tf
@@ -8,12 +8,12 @@ output "github_actions_terraform_credentials" {
   sensitive = true
 }
 
-output "github_actions_acr_push_credentials" {
-  value = {
-    client_id     = azuread_service_principal.github_actions_acr_push.client_id
-    client_secret = azuread_service_principal_password.github_actions_acr_push.value
-    subscription_id = data.azurerm_subscription.current.subscription_id
-    tenant_id       = data.azurerm_subscription.current.tenant_id
-  }
-  sensitive = true
-}
+# output "github_actions_acr_push_credentials" {
+#   value = {
+#     client_id     = azuread_service_principal.github_actions_acr_push.client_id
+#     client_secret = azuread_service_principal_password.github_actions_acr_push.value
+#     subscription_id = data.azurerm_subscription.current.subscription_id
+#     tenant_id       = data.azurerm_subscription.current.tenant_id
+#   }
+#   sensitive = true
+# }
